cd05884f1f5330a22a67fb54ac82a16e62b565c9
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-25 02:07:33 -0400

    OverviewRepository.java

---
+++ src/main/java/com/jay/stockapiv2/repositories/OverviewRepository.java
@@ -0,0 +1,7 @@
+package com.jay.stockapiv2.repositories;
+
+import com.jay.stockapiv2.models.Overview;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OverviewRepository extends CrudRepository<Overview, Long> {
+}
