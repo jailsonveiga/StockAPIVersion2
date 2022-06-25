81c3ec44013df399d8a33fc75ac4bddedee7406d
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-25 00:15:01 -0400

    ApiErrorHandling.java

---
+++ src/main/java/com/jay/stockapiv2/utils/ApiErrorHandling.java
@@ -0,0 +1,17 @@
+package com.jay.stockapiv2.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ApiErrorHandling {
+
+    public static ResponseEntity<?> genericApiError (Exception e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getClass());
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    public static ResponseEntity<?> customApiError (String message, HttpStatus status) {
+        return ResponseEntity.status(status).body(message);
+    }
+}
