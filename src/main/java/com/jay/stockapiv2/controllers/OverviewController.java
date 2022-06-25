12e06a5aaa37872175342a0e11fa245f242d463f
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-24 22:54:37 -0400

    OverviewController.java

--- src/main/java/com/jay/stockapiv2/controllers/OverviewController.java
+++ src/main/java/com/jay/stockapiv2/controllers/OverviewController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,21 @@ public class OverviewController {
         try{
 
             String url = BASE_URL + "&symbol=IBM&apikey=demo";
+
+            String responseBody = restTemplate.getForObject(url, String.class);
+
+            return ResponseEntity.ok(responseBody);
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{symbol}")
+    public ResponseEntity<?> dynamicOverview (RestTemplate restTemplate, @PathVariable String symbol) {
+        try{
+
+            String url = BASE_URL + "&symbol="+symbol+"&apikey="+env.getProperty("AV_API_KEY");
 
             String responseBody = restTemplate.getForObject(url, String.class);
 
