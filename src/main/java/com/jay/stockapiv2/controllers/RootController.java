b002eefa9e4f00fe6118e15f8ad35ee5193be395
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-24 22:43:06 -0400

    Testing the RootController

--- src/main/java/com/jay/stockapiv2/controllers/RootController.java
+++ src/main/java/com/jay/stockapiv2/controllers/RootController.java
@@ -1,11 +1,17 @@
 package com.jay.stockapiv2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
+
+    @Autowired
+    Environment env;
+
 
     @GetMapping("/")
     public ResponseEntity<?> rootRoute() {
