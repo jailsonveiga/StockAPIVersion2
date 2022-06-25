dc748f1ce09efcf74d5817fecd4237f45878f536
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-24 21:16:07 -0400

    Initial commit

---
+++ src/main/java/com/jay/stockapiv2/Stockapiv2Application.java
@@ -0,0 +1,13 @@
+package com.jay.stockapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stockapiv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stockapiv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jay/stockapiv2/Stockapiv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.stockapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stockapiv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
