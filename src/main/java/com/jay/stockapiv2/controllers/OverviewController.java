015d0cac0e01665666f7a768a1c3370c490b4fe0
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-25 00:25:26 -0400

    OverviewController.java

--- src/main/java/com/jay/stockapiv2/controllers/OverviewController.java
+++ src/main/java/com/jay/stockapiv2/controllers/OverviewController.java
@@ -1,7 +1,9 @@
 package com.jay.stockapiv2.controllers;
 
+import com.jay.stockapiv2.utils.ApiErrorHandling;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,9 +31,15 @@ public class OverviewController {
 
             return ResponseEntity.ok(alphaVantageResponse);
 
-        } catch (Exception e) {
-            System.out.println(e.getClass());
-            return ResponseEntity.internalServerError().body(e.getMessage());
+        } catch (IllegalArgumentException e) {
+
+            return ApiErrorHandling.customApiError("Error In Test Overview. Check URL used for AV Request", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+        catch (Exception e) {
+//            System.out.println(e.getClass());
+//            return ResponseEntity.internalServerError().body(e.getMessage());
+           return ApiErrorHandling.genericApiError(e);
         }
     }
 
@@ -48,10 +56,17 @@ public class OverviewController {
 
             String alphaVantageResponse = restTemplate.getForObject(url, String.class);
 
+            if(alphaVantageResponse == null) {
+                return ApiErrorHandling.customApiError("Did not receive response from AV", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            else if(alphaVantageResponse.equals("{}")) {
+                return ApiErrorHandling.customApiError("Invalid Stock Symbol: " + symbol, HttpStatus.BAD_REQUEST);
+            }
+
             return ResponseEntity.ok(alphaVantageResponse);
 
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(e.getMessage());
+            return ApiErrorHandling.genericApiError(e);
         }
     }
 
