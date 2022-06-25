96f9a60a4ba29228bf8bdd249e42b7ca2edc1cca
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-25 16:55:06 -0400

    Updated / Improve Error Handling OverviewRepository.java

--- src/main/java/com/jay/stockapiv2/utils/ApiErrorHandling.java
+++ src/main/java/com/jay/stockapiv2/utils/ApiErrorHandling.java
@@ -2,6 +2,7 @@ package com.jay.stockapiv2.utils;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 
 public class ApiErrorHandling {
 
@@ -11,7 +12,13 @@ public class ApiErrorHandling {
         return ResponseEntity.internalServerError().body(e.getMessage());
     }
 
-    public static ResponseEntity<?> customApiError (String message, HttpStatus status) {
+    public static ResponseEntity<?> customApiError (String message, int status) {
         return ResponseEntity.status(status).body(message);
+    }
+
+    public static void throwErr(int status, String message) throws HttpClientErrorException {
+
+        throw new HttpClientErrorException(HttpStatus.valueOf(status), message);
+
     }
 }
