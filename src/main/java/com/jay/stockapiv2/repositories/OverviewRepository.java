cecd45a8bf61dc3efd27f678fc4e6b2d8e5aceb1
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-25 17:38:27 -0400

    Create find route for Symbol and Exchange

--- src/main/java/com/jay/stockapiv2/repositories/OverviewRepository.java
+++ src/main/java/com/jay/stockapiv2/repositories/OverviewRepository.java
@@ -3,5 +3,14 @@ package com.jay.stockapiv2.repositories;
 import com.jay.stockapiv2.models.Overview;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OverviewRepository extends CrudRepository<Overview, Long> {
+
+    Optional<Overview> findBySymbol(String symbol);
+
+    List<Overview> findByExchange(String exchange);
+
+
 }
