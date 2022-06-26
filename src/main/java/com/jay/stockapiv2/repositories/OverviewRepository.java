f6f85fdd50e57e329d19fa06d90f2e5242eae628
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-25 23:08:47 -0400

    Create find route for Symbol, Exchange, sector etc... / Delete find route for Symbol, Exchange, sector etc...

--- src/main/java/com/jay/stockapiv2/repositories/OverviewRepository.java
+++ src/main/java/com/jay/stockapiv2/repositories/OverviewRepository.java
@@ -1,16 +1,34 @@
 package com.jay.stockapiv2.repositories;
 
 import com.jay.stockapiv2.models.Overview;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
-public interface OverviewRepository extends CrudRepository<Overview, Long> {
+@Transactional
+public interface OverviewRepository extends JpaRepository<Overview, Long> {
 
-    Optional<Overview> findBySymbol(String symbol);
-
+    List<Overview> findById(long id);
+    List<Overview> findBySymbol(String symbol);
+    List<Overview> findBySector(String sector);
+    List<Overview> findByName(String name);
+    List<Overview> findByCurrency(String currency);
+    List<Overview> findByCountry(String country);
     List<Overview> findByExchange(String exchange);
 
+    List<Overview> findByMarketCapGreaterThanEqual(long marketCap);
+    List<Overview> findByMarketCapLessThanEqual(long marketCap);
+
+    List<Overview> deleteById(long id);
+    List<Overview> deleteBySymbol(String symbol);
+    List<Overview> deleteBySector(String sector);
+    List<Overview> deleteByName(String name);
+    List<Overview> deleteByCurrency(String currency);
+    List<Overview> deleteByCountry(String country);
+    List<Overview> deleteByExchange(String exchange);
+
+    List<Overview> deleteByMarketCapGreaterThanEqual(long marketCap);
+    List<Overview> deleteByMarketCapLessThanEqual(long marketCap);
 
 }
